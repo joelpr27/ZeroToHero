db641e6fc077c603aae02aae3f042cab821be368
Jolen_19 <joelp.xir@gmail.com>
2024-11-19 04:34:15 +0100

    Coin, Roca y Bandera
    Agregado el sprite coin y su animacion con otra animacion al recogerla, agregada la roca y su script y su lanzamiento funcional con paredes rompibles y agregado el sprite de la bandera.

--- Assets/Scripts/Piedra/Piedra.cs
+++ Assets/Scripts/Piedra/Piedra.cs
@@ -5,12 +5,45 @@ using UnityEngine.Tilemaps;
 
 public class Rock : MonoBehaviour
 {
-    public string ParedTag = "Pared destructible";  
+    public string ParedTag = "ParedDestructible";
+    public string SueloTag = "Ground";
+    public string EnemyTag = "Enemy";
+    public string AndamiosTag = "Andamios";
+    public GameObject particle;
+    public Vector2 launchForce;
+    private Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
 
+        LaunchRock();
+    }
+
+    private void LaunchRock()
+    {
+        rb.AddForce(launchForce, ForceMode2D.Impulse);
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag(ParedTag))
         {
+            Instantiate(particle, transform.position, Quaternion.identity);
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.CompareTag(SueloTag))
+        {
+            Instantiate(particle, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.CompareTag(AndamiosTag))
+        {
+            Instantiate(particle, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.CompareTag(EnemyTag))
+        {
+            Instantiate(particle, transform.position, Quaternion.identity);
             Destroy(collision.gameObject);
             Destroy(gameObject);
         }
