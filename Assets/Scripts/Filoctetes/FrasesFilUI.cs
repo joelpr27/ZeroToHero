ef1739cf3addfc9a85b6102113066153ca1826f7
Joelpr27 <joel.perez.esmc@gmail.com>
2025-01-21 17:31:43 +0100

    Cambio de Fuente
    Se ha cambiado la fuente en todo el juego

--- Assets/Scripts/Filoctetes/FrasesFilUI.cs
+++ Assets/Scripts/Filoctetes/FrasesFilUI.cs
@@ -14,16 +14,16 @@ public class FrasesFilUI : MonoBehaviour
     void Start()
     {
         frases = new string[] {
-        "¡Vamos, piernas de gelatina! Hasta un caracol te ganaría en una carrera.",
-        "¿En serio? Hasta mi abuela con artritis esquivaría mejor que eso.",
-        "¡Dale más fuerte! ¡No estás acariciando un gatito, estás peleando!",
-        "¿Eso fue un golpe o una caricia? Porque la hydra ni se inmutó.",
-        "¡Vamos, héroe! ¡Mueve esos músculos de adorno antes de que te conviertan en papilla!",
-        "Si sigues así, mejor empiezo a escribir tu epitafio...",
-        "¡Por Zeus! ¡He entrenado a vacas más ágiles que tú!",
-        "¿Necesitas un mapa para encontrar la cabeza de la hydra? ¡Apunta bien, campeón!",
-        "Si el Olimpo viera esto, te mandarían de vuelta a limpiar establos.",
-        "¡No puedes fallar tanto! ¡Hasta un cíclope con un parche vería mejor que tú!"
+            "¡Vamos, piernas de gelatina! Hasta un caracol te ganaria en una carrera.",
+            "¿En serio? Hasta mi abuela con artritis esquivaria mejor que eso.",
+            "¡Dale mas fuerte! ¡No estas acariciando un gatito, estas peleando!",
+            "¿Eso fue un golpe o una caricia? Porque la hydra ni se inmuto.",
+            "¡Vamos, heroe! ¡Mueve esos musculos de adorno antes de que te conviertan en papilla!",
+            "Si sigues asi, mejor empiezo a escribir tu epitafio...",
+            "¡Por Zeus! ¡He entrenado a vacas mas ágiles que tu!",
+            "¿Necesitas un mapa para encontrar la cabeza de la hydra? ¡Apunta bien, campeon!",
+            "Si el Olimpo viera esto, te mandarian de vuelta a limpiar establos.",
+            "¡No puedes fallar tanto! ¡Hasta un ciclope con un parche veria mejor que tu!"
         };
 
     }
